556ac82bfe7450b0cce51d4b1737d21d71c3216b
AnthonyW <anthonylavel@gmail.com>
2020-07-03 23:22:19 -0500

    Adding git to project

---
+++ airport-application/src/main/java/com/grainger/aia/AirportApplication.java
@@ -0,0 +1,13 @@
+package com.grainger.aia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportApplication.class, args);
+	}
+
+}
