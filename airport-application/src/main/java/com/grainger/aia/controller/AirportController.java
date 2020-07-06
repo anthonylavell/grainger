6180468bddd57a7a00e5f254c596507dba6fec3f
AnthonyW <anthonylavel@gmail.com>
2020-07-05 21:37:48 -0500

    1. Updating Home page.

--- airport-application/src/main/java/com/grainger/aia/controller/AirportController.java
+++ airport-application/src/main/java/com/grainger/aia/controller/AirportController.java
@@ -1,12 +1,24 @@
 package com.grainger.aia.controller;
 
+import com.grainger.aia.entites.Airport;
+import com.grainger.aia.services.AirPortService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AirportController {
+
+    @Autowired
+    private AirPortService airPortService;
+
     @GetMapping("/")
-    public String displayHome(){
+    public String displayHome(Model model){
+        List<Airport> listOfAirports = airPortService.getAll();
+        model.addAttribute("airports",listOfAirports);
         return "main/index";
     }
 }
