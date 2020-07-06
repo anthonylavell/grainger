daa1029f82827fda8ba22eac4a63c51a48d31f46
AnthonyW <anthonylavel@gmail.com>
2020-07-05 20:15:54 -0500

    1. Updating properties file.

--- airport-application/src/test/java/com/grainger/aia/AirportApplicationTests.java
+++ airport-application/src/test/java/com/grainger/aia/AirportApplicationTests.java
@@ -1,9 +1,13 @@
 package com.grainger.aia;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AirportApplication.class, webEnvironment =
+		SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AirportApplicationTests {
 
 	@Test
