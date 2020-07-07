02ffef68beefd8457e5e20d169cd84e7b46f0ec2
AnthonyW <anthonylavel@gmail.com>
2020-07-07 09:50:49 -0500

    1. Commented out some code in the Calculate class.

--- airport-application/src/main/java/com/grainger/aia/uility/Calculate.java
+++ airport-application/src/main/java/com/grainger/aia/uility/Calculate.java
@@ -8,28 +8,29 @@ public final class Calculate {
                                              double long2){
         double dist = org.apache.lucene.util.SloppyMath.haversinMeters(lat1, long1, lat2, long2);
         dist *= 0.0006213711;
-        return (long)Math.round(dist);
+        return Math.round(dist);
     }
 
-    private static double getDistance(double lat1, double lon1, double lat2, double lon2){
+    /*private static double getDistance(double lat1, double lon1, double lat2,
+                                       double lon2){
         double theta = lon1 - lon2;
         double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
         dist = Math.acos(dist);
         dist = rad2deg(dist);
         dist = dist * 60 * 1.1515;
         return (dist);
-    }
+    }*/
 
     /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
     /*::  This function converts decimal degrees to radians             :*/
     /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    private static double deg2rad(double deg) {
+   /* private static double deg2rad(double deg) {
         return (deg * Math.PI / 180.0);
-    }
+    }*/
     /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
     /*::  This function converts radians to decimal degrees             :*/
     /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-    private static double rad2deg(double rad) {
+   /* private static double rad2deg(double rad) {
         return (rad * 180.0 / Math.PI);
-    }
+    }*/
 }
